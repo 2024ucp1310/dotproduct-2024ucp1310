54a05f3b26c8414b25010fd1fe2229714bc9093f
vivek kumar <vk2525583@gmail.com>
2025-11-13 12:09:18 +0530

    push commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Hello this is my git reposatory and This is My project using version control tool Git.
+My project is adding two vectors and displaying its result using Python.
---
+++ addvec.py
@@ -0,0 +1,11 @@
+def vector_add():
+	x1=int(input("Enter  value:"))
+	y1=int(input("Enter  value:"))
+	x2=int(input("Enter  value:"))
+	y2=int(input("Enter  value:"))
+	return [(x1+x2),(y1+y2)]
+L=vector_add()
+print("This is vector addition :",L[0],"i","+",L[1],"j")
+
+	
+
